3924c6a061de894977a0dfdb388bb883188e10f4
FR-Chelsea <35002212+FR-Chelsea@users.noreply.github.com>
2021-02-10 20:32:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,3 +22,4 @@ git clone https://github.com/vernesong/OpenClash  package/OpenClash
 git clone https://github.com/ntlf9t/luci-app-dnspod  package/dnspod
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/adguardhome
 git clone https://github.com/tty228/luci-app-serverchan package/serverchan
+git clone https://github.com/monokoo/luci-app-koolddns package/koolddns
